4ed126c8eac732d0c314368e814dbbfa7a74cb2a
lehuuthoct <lehuuthoct@gmail.com>
2018-09-28 12:27:28 +1000

    add initial state

--- src/components/Pomodoro/Timer.js
+++ src/components/Pomodoro/Timer.js
@@ -2,9 +2,31 @@ import React, { Component } from "react";
 
 class Timer extends Component {
 
+  constructor() {
+    super()
+    // init initial state
+    this.state = {
+      alert: {
+        type: '',
+        message: ''
+      },
+      timer: 0
+    }
+    // init times for work, short|long break
+    this.times = {
+      defaultTime: 1500, // 25 min
+      shortBreak: 300, // 5 min
+      longBreak: 900, // 15 min
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
-      <div>
+      <div className="Pomodoro">
         Timer
       </div>
     )
