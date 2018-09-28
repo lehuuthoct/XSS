a361ff937b0dab5b8516d1811e59bc82e5a67569
lehuuthoct <lehuuthoct@gmail.com>
2018-09-28 15:04:32 +1000

    init Coins component

---
+++ src/components/Coins/Coins.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Coins extends Component {
+  render() {
+    return (
+      <div>
+        Coins
+      </div>
+    )
+  }
+}
+
+export default Coins;
