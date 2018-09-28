197f282dd15378fe7ca77a43f86030d193e910e8
lehuuthoct <lehuuthoct@gmail.com>
2018-09-28 17:15:08 +1000

    add message when no component is mounted

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
+// hide unmount message
+document.getElementById("unmountMessage").style.display = "none";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
