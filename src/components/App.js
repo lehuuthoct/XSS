32f4b01120c9d2a1b28bd10534b14b4d1a30b805
lehuuthoct <lehuuthoct@gmail.com>
2018-09-28 16:09:42 +1000

    refactor App to update ES6 features tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import Coins from './Coins/Coins';
-import ErrorBoundary from './ErrorBoundary';
-import Content from './layout/Content';
-import Footer from './layout/Footer';
-import Header from './layout/Header';
+import React, { Component } from "react";
+import "./App.css";
+import Coins from "./Coins/Coins";
+import ErrorBoundary from "./ErrorBoundary";
+import Content from "./layout/Content";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-        this.testES6Features = this.testES6Features.bind(this);
-    }
+  render() {
+    // this.testES6Features();
 
-    render() {
-        // this.testES6Features();  
+    const headerInfo = {
+      title: "Pomodoro Timer",
+      url: "http://localhost:3000"
+    };
 
-        const headerInfo = {
-            title: 'Pomodoro Timer',
-            url: 'http://localhost:3000'
-        };
+    return (
+      <div className="App">
+        <ErrorBoundary>
+          <Header {...headerInfo} />
 
-        return (
-            <div className="App">
+          <Content>
+            {/* <Home /> */}
+            {/* <Todo /> */}
+            {/* <Timer /> */}
 
-                <ErrorBoundary>
-                    <Header { ...headerInfo } />
+            <Coins />
+          </Content>
 
-                    <Content>
-                        {/* <Home /> */ }
-                        {/* <Todo /> */ }
-                        {/* <Timer /> */ }
-
-                        <Coins />
-
-                    </Content>
-
-                    <Footer />
-
-                </ErrorBoundary>
-
-
-            </div>
-        );
-    }
+          <Footer />
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default App;
