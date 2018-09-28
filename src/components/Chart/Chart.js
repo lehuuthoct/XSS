775bbf54df4b7001e88d8dff9f5971d5e1ae0bf4
lehuuthoct <lehuuthoct@gmail.com>
2018-09-28 19:17:15 +1000

    add updateChart() method

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,8 +1,24 @@
+import c3 from "c3";
 import React, { Component } from "react";
 
 class Chart extends Component {
+  componentDidMount() {
+    // update chart when component is mounted
+    this.updateChart();
+  }
+
+  updateChart() {
+    c3.generate({
+      bindto: "#chart",
+      data: {
+        columns: this.props.columns,
+        type: this.props.chartType
+      }
+    });
+  }
+
   render() {
-    return <div>Chart</div>;
+    return <div id="chart" />;
   }
 }
 
