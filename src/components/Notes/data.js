0f81acf208c836b00a7cc2f9c3e484b9c358901c
lehuuthoct <lehuuthoct@gmail.com>
2018-09-28 16:09:42 +1000

    add mock data [data.js] for Notes

---
+++ src/components/Notes/data.js
@@ -0,0 +1,11 @@
+export const NoteList1 = [
+  { title: "Note 1", content: "Learn React" },
+  { title: "Note 2", content: "Learn Redux" },
+  { title: "Note 3", content: "Learn Enzyme" }
+];
+
+export const NoteList2 = [
+  { title: "Note 4", content: "Learn Jest" },
+  { title: "Note 5", content: "Learn Webpack" },
+  { title: "Note 6", content: "Learn Nodejs" }
+];
