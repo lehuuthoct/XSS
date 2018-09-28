29f1b5cc4a14153caee34340abe55fab9412f1cb
lehuuthoct <lehuuthoct@gmail.com>
2018-09-28 19:21:01 +1000

    add setBarChart() and setLineChart() to App

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,29 @@ import ErrorBoundary from "./ErrorBoundary";
 import Content from "./layout/Content";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
-import { NoteList1, NoteList2 } from "./Notes/data";
+import { NoteList2 } from "./Notes/data";
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    // Note Application
+    // this.state = {
+    //   notes: NoteList1
+    // };
+
     this.state = {
-      notes: NoteList1
+      chartType: "line"
     };
+    this.columns = [
+      ["BTC", 3000, 6000, 10000, 15000, 13000, 11000],
+      ["ETH", 2000, 3000, 5000, 4000, 3000, 940],
+      ["XRP", 100, 200, 300, 500, 400, 300]
+    ];
   }
 
   componentDidMount() {
+    //   Note Application
     //   add noteList2 after 10s
     setTimeout(() => {
       this.setState({
