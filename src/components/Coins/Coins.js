f07956f1a7cd29d74cbefdaa5cfb367af06f2763
lehuuthoct <lehuuthoct@gmail.com>
2018-09-28 15:36:25 +1000

    add UI question

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -3,8 +3,28 @@ import React, { Component } from 'react';
 class Coins extends Component {
   render() {
     return (
-      <div>
-        Coins
+      <div className='Coins'>
+
+        <h1>Buy Crypto Coins Application</h1>
+
+        {/* question  */ }
+        <div className='question'>
+          <p> How much dollars you would like to exchange for coins?</p>
+          <p>
+            <input
+              placeholder='0'
+              onChange={ this.handleOnChange }
+              type='text'
+            />
+          </p>
+        </div>
+
+        {/* answer */ }
+        <div className='answer'>
+
+        </div>
+
+
       </div>
     )
   }
