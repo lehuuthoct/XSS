fbb400a9c6ef71ea7210debb21f2ed411719e8d1
lehuuthoct <lehuuthoct@gmail.com>
2018-09-29 12:59:34 +1000

    [Number] add method handleOnChange to calculate sum of input

--- src/components/Numbers/Numbers.js
+++ src/components/Numbers/Numbers.js
@@ -7,11 +7,30 @@ class Numbers extends Component {
     results: [] // store results of the sum
   };
 
+  handleNumberChange = e => {
+    const { value } = e.target;
+    console.log("value", value);
+    // convert string value to array "12" => [1,2]
+    const numbers = Array.from(value);
+
+    // sum numbers from array [1,2] => 3
+    const result = numbers.reduce((a, b) => Number(a) + Number(b), 0);
+
+    // update local state
+    this.setState({
+      numbers: value,
+      results: [...this.state.results, result]
+    });
+  };
+
   render() {
     return (
       <div>
         Numbers
-        <input placeholder="please enter numbers" />
+        <input
+          placeholder="please enter numbers"
+          onChange={this.handleNumberChange}
+        />
         {/* result */}
         <ul>
           {this.state.results.map((result, key) => (
