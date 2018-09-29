65e1b34501ff4189db4c648338100ed35d5a0b64
lehuuthoct <lehuuthoct@gmail.com>
2018-09-29 13:19:09 +1000

    update Result to use PureComponent to avoid rerender result too many times

--- src/components/Numbers/Result.js
+++ src/components/Numbers/Result.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
-class Result extends Component {
+class Result extends PureComponent {
   render() {
     return <li>{this.props.result}</li>;
   }
