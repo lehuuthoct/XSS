068337179a70afc7dd3e6617540b97fe4a2a2949
lehuuthoct <lehuuthoct@gmail.com>
2018-09-29 14:15:09 +1000

    init responses

--- src/components/XSS/XSS.js
+++ src/components/XSS/XSS.js
@@ -1,5 +1,18 @@
 import React, { Component } from "react";
 
+const responses = [
+  { id: 1, title: "blog 1", content: "<p>learn <strong> React </strong> </p>" },
+  {
+    id: 2,
+    title: "blog 2",
+    content: `<p>Alert: <script>alert('ok')</script> </p>`
+  },
+  {
+    id: 3,
+    title: "blog 3",
+    content: `<img onmouseover="alert('not secure')" src="attack.jpg"/>`
+  }
+];
 class XSS extends Component {
   render() {
     return <div>XSS</div>;
