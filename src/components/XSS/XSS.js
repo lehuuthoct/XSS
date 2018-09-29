3a19d0d42959300c131dcd66851ffbd7f80bf34e
lehuuthoct <lehuuthoct@gmail.com>
2018-09-29 14:33:36 +1000

    add removeXSSAttacks() to remove <script> tags and event tags

--- src/components/XSS/XSS.js
+++ src/components/XSS/XSS.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import serialize from "serialize-javascript";
 
 const responses = [
   { id: 1, title: "blog 1", content: "<p>learn <strong> React </strong> </p>" },
@@ -13,9 +14,55 @@ const responses = [
     content: `<img onmouseover="alert('not secure')" src="attack.jpg"/>`
   }
 ];
+
+// init initial state of redux
+// const initialState = JSON.stringify(responses);
+const initialState = serialize(responses);
+
+// remove html tags
+const removeXSSAttacks = html => {
+  // init pattern
+  const SCRIPT_REGEX = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+  // remove <script> tags
+  while (SCRIPT_REGEX.test(html)) {
+    html = html.replace(SCRIPT_REGEX, "");
+  }
+
+  // remove events tags
+  html = html.replace(/on\w+="[^"]*/g, "");
+
+  return {
+    __html: html
+  };
+};
+
 class XSS extends Component {
   render() {
-    return <div>XSS</div>;
+    // parsed response
+    console.log("stringified state [string]", initialState);
+
+    const posts = JSON.parse(initialState);
+    console.log("parsed posts ", posts);
+
+    return (
+      <div className="XSS">
+        XSS
+        {posts.map((
+          blog,
+          key // title
+        ) => (
+          <div key={key}>
+            <h4> {blog.title} </h4>
+            {/* secure code */}
+            <p>
+              <strong>{blog.content}</strong>
+            </p>
+            {/* insecure code */}
+            <p dangerouslySetInnerHTML={removeXSSAttacks(blog.content)} />
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
