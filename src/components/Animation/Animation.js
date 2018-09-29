779b70d814a0bfb40049d06e05fe0a4536a5ed96
lehuuthoct <lehuuthoct@gmail.com>
2018-09-29 12:17:55 +1000

    add style to hide/show text to componentWillUpdate() & update UI text & animation

--- src/components/Animation/Animation.js
+++ src/components/Animation/Animation.js
@@ -7,18 +7,37 @@ class Animation extends Component {
       show: false
     };
   }
+
+  componentWillUpdate(newProps, newState) {
+    console.log("newProps", newProps, newState);
+    if (!newState.show) {
+      document.getElementById("fade").style = "opacity: 1";
+    } else {
+      document.getElementById("fade").style = "opacity: 0";
+    }
+  }
+
   toggleCollapse = () => {
     this.setState({
       show: !this.state.show
     });
   };
+
   render() {
     return (
-      <div>
-        Animation
+      <div className="Animation">
+        {/* Toggle Animation */}
         <button onClick={this.toggleCollapse}>
           Toggle {this.state.show ? "Collapse" : "Expand"}
         </button>
+
+        {/* Text */}
+        <div
+          id="fade"
+          className={this.state.show ? "transition show" : "transition"}
+        >
+          Text will disappear
+        </div>
       </div>
     );
   }
