65c12ea0fe2bdd613f66f4b210899e85dc580e73
lehuuthoct <lehuuthoct@gmail.com>
2018-09-29 12:00:45 +1000

    add toggleCollapse() function & update UI button

--- src/components/Animation/Animation.js
+++ src/components/Animation/Animation.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 
 class Animation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false
+    };
+  }
+  toggleCollapse = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
   render() {
     return (
       <div>
         Animation
-        <button>Toggle Animation</button>
+        <button onClick={this.toggleCollapse}>
+          Toggle {this.state.show ? "Collapse" : "Expand"}
+        </button>
       </div>
     );
   }
